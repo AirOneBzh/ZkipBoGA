4e6a4021e0e290c031bfb63cf20e97c1b6b9b23c
Lce987 <lce987@live.fr>
2017-11-11 17:07:50 +0100

    tt

--- jeu.c
+++ jeu.c
@@ -13,15 +13,17 @@ typedef struct {
   num_joueurs n;
 } joueurs ;
 
+typedef
+
 typedef struct {
-  char nom[30][30];    //nom du Jeu
+  strchar nom[30];    //nom du Jeu
   int nbj;
-  int ia[4];       // ia[0] nb, ia[1,2,3]=1,2,3 niveau de chacune ia
+  int ia[4];       // ia[0] nombre, ia[1,2,3]=1,2,3 niveau de chacune ia
 } options;
 
-void menufen(int nbmenu,options *o){ // menuc nombre d'entrees, menuv titre menu
+void menufen(int nbmenu,options o){ // menuc nombre d'entrees, menuv titre menu
   int c,x,y;
-  char titre[20];
+  char titre[30];
   MLV_create_window("Menu","Menu",800,900);
 
   MLV_Image* header = MLV_load_image("header.png");
@@ -30,10 +32,11 @@ void menufen(int nbmenu,options *o){ // menuc nombre d'entrees, menuv titre menu
   MLV_Font* font=MLV_load_font("pricedown.ttf",60);
   MLV_Font* devil60=MLV_load_font("Devil Breeze Bold.ttf",60);
   MLV_Font* devil80=MLV_load_font("Devil Breeze Bold.ttf",90);
-  strcpy(titre,o->nom[0]);
-  sprintf(o->nom[0],"Lancer %s",titre);
-  sprintf(o->nom[1],"Options");
-  sprintf(o->nom[2],"Bots");
+
+  sprintf(o.nom[0],"Lancer %s",o.nom[0]);
+  sprintf(o.nom[1],"Options");
+  sprintf(o.nom[2],"Bots");
+  nbmenu+=2;
   MLV_clear_window(fond);
   MLV_draw_image(header,30,15);
   MLV_draw_text_with_font(150,15,titre,font,MLV_COLOR_WHITE);
@@ -42,16 +45,13 @@ void menufen(int nbmenu,options *o){ // menuc nombre d'entrees, menuv titre menu
   MLV_draw_text_with_font(683,10,"a",devil80,MLV_COLOR_BLUE);           // AirOne
   for(c=0;c<nbmenu;c++){
     MLV_draw_filled_rectangle(40,140+100*c,720,80,MLV_COLOR_WHITE);
-    MLV_draw_text_with_font(80,140+100*c,o->nom[c],font,fond);
+    MLV_draw_text_with_font(80,140+100*c,o.nom[c],font,fond);
   }
 
   MLV_actualise_window();
   MLV_wait_mouse(&x,&y);
   MLV_free_window();
 
-  MLV_actualise_window();
-  MLV_wait_mouse(&x,&y);
-  MLV_free_window();
 
 }
 
@@ -68,7 +68,6 @@ int fenetre (int l,int h){
 int main (int argc, char *argv[]){
   options o;
   sprintf(o.nom[0],"ZkipBoGA");
-
-  menufen(2,&o);
+  menufen(1,o);
   return 1;
 }
