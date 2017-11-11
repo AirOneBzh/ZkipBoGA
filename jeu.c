8e0a94d562c8c6b80b4d37bbb78b7d2c7910ea0d
Lce987 <lce987@live.fr>
2017-11-11 22:28:22 +0100

    module jeu à préparer

--- jeu.c
+++ jeu.c
@@ -7,36 +7,37 @@
 #include <MLV/MLV_all.h>
 #include "jeu.h"
 
-void menufen(int nbmenu,options o){ // menuc nombre d'entrees, menuv titre menu
-int c,x,y;
-char titre[30];
-MLV_create_window("Menu","Menu",800,900);
+// menuc nombre d'entrees, menuv titre menu
+void menufen(int nbmenu,options o){
+  int c,x,y;
+  char titre[30];
+  MLV_create_window("Menu","Menu",800,900);
 
-MLV_Image* header = MLV_load_image("header.png");
-MLV_resize_image(header,100,100);
-MLV_Color fond = MLV_rgba(40,40,40,255);
-MLV_Font* font=MLV_load_font("pricedown.ttf",60);
-MLV_Font* devil60=MLV_load_font("Devil Breeze Bold.ttf",60);
-MLV_Font* devil80=MLV_load_font("Devil Breeze Bold.ttf",90);
+  MLV_Image* header = MLV_load_image("header.png");
+  MLV_resize_image(header,100,100);
+  MLV_Color fond = MLV_rgba(40,40,40,255);
+  MLV_Font* font=MLV_load_font("pricedown.ttf",60);
+  MLV_Font* devil60=MLV_load_font("Devil Breeze Bold.ttf",60);
+  MLV_Font* devil80=MLV_load_font("Devil Breeze Bold.ttf",90);
 
-sprintf(o.nom[0],"Lancer %s",o.nom[0]);
-sprintf(o.nom[1],"Options");
-sprintf(o.nom[2],"Bots");
-nbmenu+=2;
-MLV_clear_window(fond);
-MLV_draw_image(header,30,15);
-MLV_draw_text_with_font(150,15,titre,font,MLV_COLOR_WHITE);
-MLV_draw_text_with_font(500,15,"ZNG",devil60,MLV_COLOR_RED);          // T. ZANGA :)
-MLV_draw_text_with_font(640,10,"c",devil80,MLV_COLOR_DARKGREEN);      // Cesarus
-MLV_draw_text_with_font(683,10,"a",devil80,MLV_COLOR_BLUE);           // AirOne
-for(c=0;c<nbmenu;c++){
-  MLV_draw_filled_rectangle(40,140+100*c,720,80,MLV_COLOR_WHITE);
-  MLV_draw_text_with_font(80,140+100*c,o.nom[c],font,fond);
-}
+  sprintf(o.nom[0],"Lancer %s",o.nom[0]);
+  sprintf(o.nom[1],"Options");
+  sprintf(o.nom[2],"Bots");
+  nbmenu+=2;
+  MLV_clear_window(fond);
+  MLV_draw_image(header,30,15);
+  MLV_draw_text_with_font(150,15,titre,font,MLV_COLOR_WHITE);
+  MLV_draw_text_with_font(500,15,"ZNG",devil60,MLV_COLOR_RED);          // T. ZANGA :)
+  MLV_draw_text_with_font(640,10,"c",devil80,MLV_COLOR_DARKGREEN);      // Cesarus
+  MLV_draw_text_with_font(683,10,"a",devil80,MLV_COLOR_BLUE);           // AirOne
+  for(c=0;c<nbmenu;c++){
+    MLV_draw_filled_rectangle(40,140+100*c,720,80,MLV_COLOR_WHITE);
+    MLV_draw_text_with_font(80,140+100*c,o.nom[1],font,fond);
+  }
 
-MLV_actualise_window();
-MLV_wait_mouse(&x,&y);
-MLV_free_window();
+  MLV_actualise_window();
+  MLV_wait_mouse(&x,&y);
+  MLV_free_window();
 
 
 }
